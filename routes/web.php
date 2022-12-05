e651a3910244fc18340b6e40ac6816fec43ed39d
Nitish Suri <42737008+NitishSuri@users.noreply.github.com>
2022-12-05 13:21:31 +0530

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'StudentController@index');
+Route::get('/display', 'StudentController@display')->name('display');
+Route::get('/edit/{id}', 'StudentController@edit')->name('edit');
