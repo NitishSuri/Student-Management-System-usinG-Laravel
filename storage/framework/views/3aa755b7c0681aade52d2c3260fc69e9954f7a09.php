965904fe57108a4fa26b6854988f090ccc5276ad
Nitish Suri <42737008+NitishSuri@users.noreply.github.com>
2022-12-05 13:23:19 +0530

    Add files via upload

---
+++ storage/framework/views/3aa755b7c0681aade52d2c3260fc69e9954f7a09.php
@@ -0,0 +1,8 @@
+
+<?php $__env->startSection('content'); ?>
+<div class="container-fluid">
+<display-student></display-student>
+</div>
+  
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\work\laravel-trainings2\resources\views/display.blade.php ENDPATH**/ ?>
